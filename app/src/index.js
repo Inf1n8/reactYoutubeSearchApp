a0bd963733905e5ecf056cd4001b0d1c0115bb61
Inf1n8 <vikrame1999@gmail.com>
2018-05-08 20:30:13 +0530

    fetching videosusing API and printing videolist

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './searchBar';
+import VideoList from './videoList';
+import YTSearch from 'youtube-api-search';
 
 const APIkey='AIzaSyAm8kGp_X1ZdZ6UqeIn9Fs16TjXzrDLzho';
 
+
+
 //Create a new component. This component produces some HTML code
-const App = () => {
-    return <div><ol><li>Chicken</li><li>Chicken</li><li>Chicken</li><li>Chicken</li><li>Chicken</li></ol></div>;
-};
+
+class App extends Component{
+    constructor(props)
+    {
+        super(props);
+
+        this.state={ videos : [] };
+
+        YTSearch({key: APIkey, term : 'vivegam'},(videos) => {
+            console.log(videos);
+            this.setState({ videos : videos });
+        });
+    }
+    render(){
+        return (<div>
+            <SearchBar/>
+            <VideoList videos={this.state.videos}/>
+        </div>);
+    }
+}
 
 //Take the components generated and add it to DOM
 ReactDOM.render(<App/>,document.querySelector('.container'));
